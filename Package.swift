8dcce4c55fed9b399c9ec94705569847da219ea9
Lyubomir Marinov <lyubomir@Lyubomirs-MacBook-Air.local>
2021-08-29 19:54:43 +0300

    Fix RxSwift version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "RxCoreLocation", targets: ["RxCoreLocation"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0")
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
     ],
     targets: [
         .target(
